e5b617c6e09a577b83e72b82278a86461d4383c7
Frank Ochino <ochinomaniac@gmail.com>
2024-08-04 12:39:19 +0300

    git commit --al

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {};
+module.exports = {
+    experimental: {
+      serverComponents: true,
+    },
+  };
+  
 
 export default nextConfig;
