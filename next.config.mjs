8706bac297edba15c0b811ab14bf08e2365a8969
Frank Ochino <ochinomaniac@gmail.com>
2024-08-05 09:48:07 +0300

    committing and pushing all changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {};
-module.exports = {
-    experimental: {
-      serverComponents: true,
-    },
-  };
-  
 
 export default nextConfig;
